07333c5ce9e84721dc58e54227cabb2b5aecd9dd
wiusxt <ashatovanas65@gmail.com>
2022-06-21 14:34:41 +0600

    usersSlice file are updated

--- src/usersSlice.js
+++ src/usersSlice.js
@@ -1,4 +1,10 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
+
+export const loadUsers = createAsyncThunk('users/load', async (arg, thunkAPI) => {
+ const response = await axios.get('https://jsonplaceholder.typicode.com/users');
+ return response.data;
+});
 
 const usersSlice = createSlice({
 name: "users",
